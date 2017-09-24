f8c96b6cdc18bbcc254670eae6d855f4bf624bd8
kassadube <noamsrb@yahoo.com>
2017-09-24 07:27:56 +0300

    ss

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -13,7 +13,7 @@ namespace aspnetcore_rest_api_with_dapper.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
